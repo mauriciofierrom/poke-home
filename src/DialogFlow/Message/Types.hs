cab1e33f93a54e1f9d12e132c7dd8576ac7e0a8c
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-04-12 21:28:56 -0500

    Basic pumbling for a test. Must finish serving the API first.

---
+++ src/DialogFlow/Message/Types.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module DialogFlow.Message.Types
+  ( Button(..)
+  , MText(..)
+  ) where
+
+import Data.Aeson (FromJSON, parseJSON, withObject, (.:))
+import GHC.Generics
+
+newtype MText = MText { tText :: Maybe [String] } deriving (Eq, Show)
+
+instance FromJSON MText where
+  parseJSON = withObject "text" $ \o -> do
+    firstText <- o .: "text"
+    tText <- firstText .: "text"
+    return MText{..}
+
+data Image = Image { iImageUri :: Maybe String
+                     , accessibilityText :: Maybe String
+                     } deriving (Eq, Generic, Show)
+
+data QuickReply = QuickReply { title :: Maybe String
+                             , quickReplies :: Maybe [String]
+                             } deriving (Eq, Generic, Show)
+
+data Button = Button { buttonText :: Maybe String
+                     , buttonPostback :: Maybe String
+                     } deriving (Eq, Generic, Show)
+
+data Card = Card { cCardTitle :: Maybe String
+                 , cSubtitle :: Maybe String
+                 , cImageUri :: Maybe String
+                 , cButtons :: Maybe [Button]
+                 } deriving (Eq, Generic, Show)
+
+data SimpleResponse =
+  SimpleResponse { textToSpeech :: String -- these fields are mutually exclusive vv
+                 , ssml :: String -- ^^ these fields are mutually exclusive ^^
+                 , displayText :: Maybe String
+                 } deriving (Eq, Generic, Show)
+
+data BasicCard =
+  BasicCard { bsTitle :: Maybe String
+            , bsSubtitle :: Maybe String
+            , bsFormattedText :: String
+            , bsImage :: Maybe Image
+            , bsButtons :: [Button]
+            } deriving (Eq, Generic, Show)
+
+newtype Suggestions =
+  Suggestions { sugTitle :: String } deriving (Eq, Generic, Show)
+
+data LinkOutSuggestion =
+  LinkOutSuggestion { losDestinationName :: String
+                    , losUri :: String
+                    } deriving (Eq, Generic, Show)
+
+data SelectItemInfo =
+  SelectItemInfo { siiKey :: String
+                 , siiSynonyms :: Maybe [String] } deriving (Eq, Generic, Show)
+
+data Item =
+  Item { iInfo :: SelectItemInfo
+       , iTitle :: String
+       , iDescription :: Maybe String
+       , iImage :: Maybe Image
+       } deriving (Eq, Generic, Show)
+
+data ListSelect =
+  ListSelect { lsTitle :: Maybe String
+             , lsItems :: [Item]
+             } deriving (Eq, Generic, Show)
+
+newtype CarouselSelect =
+  CarouselSelect { csItems :: [Item] } deriving (Eq, Generic, Show)
--- src/Types.hs
+++ src/Types.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
-module Types where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Data.Aeson (FromJSON)
-import Data.HashMap.Lazy (HashMap)
+module Types where
+import Data.Aeson (FromJSON, parseJSON, withObject, (.:))
 import GHC.Generics
+
+import qualified Data.Map as M
+
+import qualified DialogFlow.Message.Types as DF
 
 data DFIntent =
   DFIntent { intentName :: String
@@ -15,7 +20,7 @@ instance FromJSON DFIntent
 data DFOutputContext =
   DFOutputContext { name :: String
                   , lifespanCount :: Int
-                  , outputContextparameters :: HashMap String String
+                  , outputContextparameters :: M.Map String String
                   }
                   deriving (Eq, Generic, Show)
 
@@ -23,10 +28,12 @@ instance FromJSON DFOutputContext
 
 data DFQueryResult =
   DFQueryResult { queryText :: String
-                , parameters :: HashMap String String
+                , parameters :: M.Map String String
                 , allRequiredParamsPresent :: Bool
                 , fulfillmentText :: String
-                , fulfillmentMessages :: HashMap String String
+                -- disabled for now, as it doesn't seem necessary for the
+                -- current implementation
+                -- , fulfillmentMessages :: M.Map String String
                 , outputContexts :: DFOutputContext
                 , intent :: DFIntent
                 , intentDetectionConfidence :: Int
@@ -45,4 +52,21 @@ data DFRequest =
 
 instance FromJSON DFRequest
 
-data DFResponse
+{- Doesn't use the following parameters:
+   - outputContexts[]
+   - followupEventInput[]
+-}
+data DFResponse =
+  DFResponse { dfrFulfillmentText :: Maybe String
+             , dfrFulfillmentMessages :: Maybe [DF.MText] -- check how to use an unified data type
+             , dfrSource :: Maybe String
+             -- , dfrPayload :: M.Map String String
+             } deriving (Eq, Show)
+
+instance FromJSON DFResponse where
+  parseJSON = withObject "response" $ \d -> do
+    dfrFulfillmentText <- d .: "fulfillmentText"
+    dfrFulfillmentMessages <- d .: "fulfillmentMessages"
+    dfrSource <- d .: "source"
+    -- dfrPayload <- d .: "payload"
+    return DFResponse{..}
