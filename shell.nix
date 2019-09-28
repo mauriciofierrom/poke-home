62844f02910d85fffe81268ed2b8b05955b60e5f
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-09-28 14:56:28 -0500

    Use with

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.shellFor {
-  packages = ps: [ (import ./release.nix).pokehome ];
-  buildInputs = [ pkgs.haskellPackages.ghcid ];
-}
+with pkgs; {
+  shell = haskellPackages.shellFor {
+    packages = ps: [ (import ./release.nix).pokehome ];
+    buildInputs = [ haskellPackages.ghcid ];
+  };
+}.shell
